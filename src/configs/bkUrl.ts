a7a75bc221fbf595534a091b83a0ba8e63b7048f
tex <afaqhafeez0@gmail.com>
2024-07-11 00:33:56 +0500

    bk url

--- src/configs/bkUrl.ts
+++ src/configs/bkUrl.ts
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === "production"
-  ? 'https://node193354-shop-manager.in1.cloudjiffy.net/api'
+  ? 'https://node209305-shop-manager.in1.cloudjiffy.net/api'
   : 'http://127.0.0.1:4000/api';
 
 export default baseUrl;
